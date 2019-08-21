b57c8d6c56ec63a49ea5b25b3daa8c87ddf01662
Todd Mantell <tmantell99@yahoo.com>
2019-08-20 20:20:03 -0500

    fixed issue with missing key formapped Stonks

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import getStonks from "./data/getStonks";
+import Stonk from "./components/stonk";
 
 export default function Dashboard() {
   const [stonks, setStonks] = useState([]);
@@ -18,9 +19,9 @@ export default function Dashboard() {
   return (
     <main>
       {stonks.length &&
-        stonks.map((stonk, index) => (
-          <div key={index}>{stonk.companyName}</div>
-        ))}
+        stonks.map((stonk, index) => {
+          return <Stonk key={`stonk-${index}`} {...stonk} />;
+        })}
     </main>
   );
 }
--- src/components/stonk.js
+++ src/components/stonk.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function Stonk() {
-  return <div />;
+export default function Stonk({ companyName, localTicker }) {
+  return (
+    <figure>
+      <div>Company Name: {companyName}</div>
+      <div>Ticker Symbol: {localTicker}</div>
+    </figure>
+  );
 }
