9358c2670b0191e3dfc41a55646b3491eff33877
Todd Mantell <tmantell99@yahoo.com>
2019-08-21 21:30:23 -0500

    added react-flip for flippable cards

--- src/components/stonk.js
+++ src/components/stonk.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 
 export default function Stonk({
   companyName,
@@ -11,83 +12,90 @@ export default function Stonk({
   localTicker
 }) {
   return (
-    <figure className="stonk-card">
-      <div className="stonk-attribute">
-        Company Name:{" "}
-        <span style={{ textDecoration: "underline" }}>{companyName}</span>
-      </div>
-      <hr />
-      <div className="stonk-attribute">
-        Ticker Symbol: <span>{localTicker}</span>
-      </div>
-      <hr />
-      <div className="stonk-attribute">
-        Latest Price: <span>{latestPrice}</span>
-      </div>
-      <hr />
-      <div className="stonk-attribute">
-        Graham Number:{" "}
-        <span
-          style={
-            grahamNumber > latestPrice ? { color: "green" } : { color: "red" }
-          }
-        >
-          {grahamNumber}
-        </span>
-      </div>
-      <hr />
-      <div className="stonk-attribute">
-        Forward Number:{" "}
-        <span
-          style={
-            forwardGrahamFormulaNumber > latestPrice
-              ? { color: "green" }
-              : { color: "red" }
-          }
-        >
-          {forwardGrahamFormulaNumber}
-        </span>
-      </div>
-      <hr />
-      <div className="stonk-attribute">
-        Forward Number (Conservative):{" "}
-        <span
-          style={
-            forwardConservativeGrahamFormulaNumber > latestPrice
-              ? { color: "green" }
-              : { color: "red" }
-          }
-        >
-          {forwardConservativeGrahamFormulaNumber}
-        </span>
-      </div>
-      <hr />
-      <div className="stonk-attribute">
-        Past Number:{" "}
-        <span
-          style={
-            pastGrahamFormulaNumber > latestPrice
-              ? { color: "green" }
-              : { color: "red" }
-          }
-        >
-          {pastGrahamFormulaNumber}
-        </span>
-      </div>
-      <hr />
-      <div className="stonk-attribute">
-        Past Number (Conservative):{" "}
-        <span
-          style={
-            pastConservativeGrahamFormulaNumber > latestPrice
-              ? { color: "green" }
-              : { color: "red" }
-          }
-        >
-          {pastConservativeGrahamFormulaNumber}
-        </span>
-      </div>
-      <hr />
-    </figure>
+    <Flippy
+      flipOnHover={false}
+      flipOnClick={true}
+      flipDirection="horizontal"
+      style={{ width: "350px", height: "auto" }}
+    >
+      <FrontSide className="stonk-card">
+        <div className="stonk-attribute stonk-attribute__top">
+          Company Name:{" "}
+          <span style={{ textDecoration: "underline" }}>{companyName}</span>
+        </div>
+        <hr />
+        <div className="stonk-attribute">
+          Ticker Symbol: <span>{localTicker}</span>
+        </div>
+        <hr />
+        <div className="stonk-attribute">
+          Latest Price: <span>{latestPrice}</span>
+        </div>
+      </FrontSide>
+      <BackSide>
+        <div className="stonk-attribute">
+          Graham Number:{" "}
+          <span
+            style={
+              grahamNumber > latestPrice ? { color: "green" } : { color: "red" }
+            }
+          >
+            {grahamNumber}
+          </span>
+        </div>
+        <hr />
+        <div className="stonk-attribute">
+          Forward Number:{" "}
+          <span
+            style={
+              forwardGrahamFormulaNumber > latestPrice
+                ? { color: "green" }
+                : { color: "red" }
+            }
+          >
+            {forwardGrahamFormulaNumber}
+          </span>
+        </div>
+        <hr />
+        <div className="stonk-attribute">
+          Forward Number (Conservative):{" "}
+          <span
+            style={
+              forwardConservativeGrahamFormulaNumber > latestPrice
+                ? { color: "green" }
+                : { color: "red" }
+            }
+          >
+            {forwardConservativeGrahamFormulaNumber}
+          </span>
+        </div>
+        <hr />
+        <div className="stonk-attribute">
+          Past Number:{" "}
+          <span
+            style={
+              pastGrahamFormulaNumber > latestPrice
+                ? { color: "green" }
+                : { color: "red" }
+            }
+          >
+            {pastGrahamFormulaNumber}
+          </span>
+        </div>
+        <hr />
+        <div className="stonk-attribute stonk-attribute__bottom">
+          Past Number (Conservative):{" "}
+          <span
+            style={
+              pastConservativeGrahamFormulaNumber > latestPrice
+                ? { color: "green" }
+                : { color: "red" }
+            }
+          >
+            {pastConservativeGrahamFormulaNumber}
+          </span>
+        </div>
+      </BackSide>
+    </Flippy>
   );
 }
