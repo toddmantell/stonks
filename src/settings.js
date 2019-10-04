a71abb7f2b488ce61225822e4f699a2c66c0282f
Todd Mantell <tmantell99@yahoo.com>
2019-10-03 21:25:37 -0500

    updated the api url to the temporary 'live' version served by Lambda"

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 export default {
-  PROD_API_URL: "https://myprodlambdainstance",
+  PROD_API_URL: "https://sf3ssnupuc.execute-api.us-east-1.amazonaws.com/dev",
   DEV_API_URL: "http://localhost:5000"
 };
