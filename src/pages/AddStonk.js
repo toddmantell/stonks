0a582b20f407090db403e6d58f247ba146162903
Todd Mantell <tmantell99@yahoo.com>
2019-10-05 22:09:36 -0500

    added a rough start for fetching/storing the symbols for a typeahead in AddStonk

--- src/pages/AddStonk.js
+++ src/pages/AddStonk.js
@@ -3,19 +3,52 @@ import { useDebounce } from "use-debounce";
 import { getDevOrProdAPIURL } from "../data/getStonks";
 
 export default function CheckStonk() {
+  const apiUrl = getDevOrProdAPIURL();
   const [stonkTicker, setStonkTicker] = useState("");
   const [stonk, setStonk] = useState(false);
   const [growthRate, setGrowthRate] = useState(0);
   const debounceTime = 1000;
 
+  //ignore error for now, we'll do an array.find for symbols in a typeahead
+  const [symbols, setSymbols] = useState([]);
+
   const [debouncedTicker] = useDebounce(stonkTicker, debounceTime);
 
   useEffect(() => {
+    getSymbols();
     if (debouncedTicker) getStonk(debouncedTicker);
 
+    // get the symbols (lazily) for a smooth typeahead experience (typeahead not yet implemented)
+    async function getSymbols() {
+      if (localStorage.symbols)
+        return setSymbols(JSON.parse(localStorage.symbols));
+
+      if (
+        localStorage.symbols &&
+        localStorage.symbols.lastUpdated > Date.now() - 86400000
+      ) {
+        // IEX updates the symbols every day at 7:45 Eastern
+        // Check to see if the data in localStorage is older than this morning at 7:45 or 7:45 am
+        // if 24 hours have not passed since 7:45 yet
+        // Might also be a good idea to have some mechanism in the back end that retrieves the data and/or stores it in a db
+      }
+
+      try {
+        const symbolsResponse = await fetch(`${apiUrl}/symbols`);
+        const symbols = await symbolsResponse.json();
+        localStorage.symbols = JSON.stringify({
+          ...symbols,
+          lastUpdated: Date.now()
+        });
+        setSymbols(symbols);
+      } catch (error) {
+        console.log(`An error occurred while fetching symbols: ${error}`);
+      }
+    }
+
+    // since we are going to use a typeahead, this will be used when the actual stock is chosen
     async function getStonk(ticker) {
       console.log(`getting ${ticker}...`);
-      const apiUrl = getDevOrProdAPIURL();
 
       try {
         const response = await fetch(`${apiUrl}/quote/${ticker}`);
