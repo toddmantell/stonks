ccd712b9e5232dbdcf70a6e28f672849d9566709
Todd Mantell <tmantell99@yahoo.com>
2019-10-16 09:56:43 -0500

    added media queries and updated grid; updated Header styling; added links for navigation; moved inline styling to stylesheet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="App-header">
-      <div>STONKS!</div>
-      <div style={{ fontSize: "16px" }}>
-        *The name of this app is a reference to{" "}
+      <h2 className="heading">STONKS!</h2>
+      <div className="explanation-text">
+        (The name of this app is a reference to{" "}
         <a
           href="https://knowyourmeme.com/memes/stonks"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "white" }}
         >
-          the "Stonks" meme.
+          the "Stonks" meme.)
         </a>
       </div>
+      <Link to="/">Home/Dashboard</Link>
+      <Link to="/addStonk">Add Stonks</Link>
     </header>
   );
 }
