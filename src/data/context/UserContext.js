96e2e243208465a559abae7ec3050a9a9da12132
Todd Mantell <tmantell99@yahoo.com>
2019-11-05 10:17:41 -0600

    fixed issue with always pulling from localStorage, also cleaned up UserContext so checkForUpdates is more SRP

--- src/data/context/UserContext.js
+++ src/data/context/UserContext.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import getStonks, { getDevOrProdAPIURL } from "../getStonks";
+import { getDevOrProdAPIURL } from "../getStonks";
 import { get, post } from "../fetchWrapper";
 
 const UserContext = React.createContext();
 export default UserContext;
 
-//the old class way
 export class UserProvider extends Component {
   state = {
     user: {},
@@ -21,53 +20,43 @@ export class UserProvider extends Component {
       const userResult = await get(
         `${this.apiUrl}/api/user/3a2d78d0-fccb-11e9-89d5-ed165fddd755`
       );
-      // We can probably get rid of the getStonks method since we're storing them with the user
-      // const result = await getStonks();
-
-      // if (!userResult.stonks.length) {
-      //   // we probably would rather have a better UX for this, but for a ProofOfC this suffices
-      //   window.alert("Failed to retrieve stonks. You are viewing stale data.");
-      //   return this.setState({ stonks: JSON.parse(localStorage.stonks) });
-      // }
 
       const stonks = await get(
         `${this.apiUrl}/api/stock/dashboard/${userResult.id}`
       );
 
-      return this.checkForUpdatedStonks(userResult, stonks);
+      const updated = this.checkForUpdatedStonks(stonks);
+
+      if (updated === false && localStorage.stonks)
+        return this.setState({
+          user: userResult,
+          stonks: JSON.parse(localStorage.stonks),
+          isLoading: false
+        });
+
+      this.setState({ user: userResult, stonks, isLoading: false });
+      // stringify is necessary because items in local storage are stored as strings
+      localStorage.stonks = JSON.stringify(stonks);
     } catch (error) {
       console.log(error);
     }
   }
 
-  checkForUpdatedStonks(user, stonks) {
-    const { updated } = this.state;
+  checkForUpdatedStonks(stonks) {
+    if (localStorage.stonks) {
+      for (let i = 0; i < stonks.length; i += 1) {
+        const stonksInLocalStorage =
+          localStorage.stonks && JSON.parse(localStorage.stonks);
 
-    // if (localStorage.stonks) {
-    //   for (let i = 0; i < stonks.length; i += 1) {
-    //     const stonksInLocalStorage =
-    //       localStorage.stonks && JSON.parse(localStorage.stonks);
+        const currentStonkInStorage = stonksInLocalStorage.find(
+          stonk => stonk.symbol === stonks[i].symbol
+        );
 
-    //     const currentStonkInStorage = stonksInLocalStorage.find(
-    //       stonk => stonk.localTicker === stonks[i].localTicker
-    //     );
-
-    //     if (currentStonkInStorage.latestPrice !== stonks[i].latestPrice) {
-    //       this.setState({ updated: true });
-    //     }
-    //   }
-    // }
-
-    // if (updated === false && localStorage.stonks)
-    //   return this.setState({
-    //     user,
-    //     stonks: JSON.parse(localStorage.stonks),
-    //     isLoading: false
-    //   });
-
-    this.setState({ user, stonks, isLoading: false });
-    // stringify is necessary because items in local storage are stored as strings
-    localStorage.stonks = JSON.stringify(stonks);
+        if (currentStonkInStorage.latestPrice !== stonks[i].latestPrice) {
+          return true;
+        }
+      }
+    }
   }
 
   addStonkToStonks = async (userId, stonk) => {
@@ -91,64 +80,3 @@ export class UserProvider extends Component {
     );
   }
 }
-
-// hooks way (this is failing, possibly at the useEffect level)
-// function UserContextProvider(props) {
-//   console.log("context is executing");
-
-//   const [stonks, setStonks] = useState([]);
-//   const [updated, setUpdated] = useState(false);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const result = await getStonks();
-
-//         console.log("result", result);
-
-//         if (!result.length) {
-//           // we probably would rather have a better UX for this, but for a ProofOfC this suffices
-//           window.alert(
-//             "Failed to retrieve stonks. You are viewing stale data."
-//           );
-//           return setStonks(JSON.parse(localStorage.stonks));
-//         }
-
-//         return checkForUpdatedStonks(result);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-
-//     function checkForUpdatedStonks(fetchResult) {
-//       for (let i = 0; i < fetchResult.length; i += 1) {
-//         const stonksInLocalStorage =
-//           localStorage.stonks && JSON.parse(localStorage.stonks);
-
-//         const currentStonkInStorage = stonksInLocalStorage.find(
-//           stonk => stonk.localTicker === fetchResult[i].localTicker
-//         );
-
-//         if (currentStonkInStorage.latestPrice !== fetchResult[i].latestPrice) {
-//           setUpdated(true);
-//         }
-//       }
-
-//       if (!updated) return setStonks(JSON.parse(localStorage.stonks));
-
-//       setStonks(fetchResult);
-//       // stringify is necessary because items in local storage are stored as strings
-//       localStorage.stonks = JSON.stringify(fetchResult);
-//     }
-
-//     fetchData();
-//   }, []);
-
-//   console.log("stonks in context", stonks);
-
-//   return (
-//     <UserContext.Provider value={{ stonks, updated }}>
-//       {props.children}
-//     </UserContext.Provider>
-//   );
-// }
