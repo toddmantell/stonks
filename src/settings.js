fec7e914966ef31bfa80ac22cab439290e902734
Todd Mantell <tmantell99@yahoo.com>
2020-05-17 17:26:51 -0500

    small fix

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,4 @@
 export default {
   PROD_API_URL: "https://sf3ssnupuc.execute-api.us-east-1.amazonaws.com/dev",
-  DEV_API_URL: "http://localhost:5000"
-  // real dev"http://localhost:5000"
+  DEV_API_URL: "http://localhost:5000",
 };
