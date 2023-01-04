e672fca6769aabfac76a318f0330b731b177ad77
todder02 <tmantell99@yahoo.com>
2023-01-03 19:48:43 -0600

    added ability to sort cards

---
+++ src/components/StonkCards.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react';
+import Stonk from "./Stonk";
+import NCAVStonk from "./NCAVStonk";
+
+export default function StonkCards({stonks = [], options = [], removeStonk = () => ''}) {
+
+	const [sortValue, setSortValue] = useState(-1);
+	const [sortedStonks, setSortedStonks] = useState(stonks);
+
+	function sortByUndervalued (stonka, stonkb) {
+		return (stonka.latestPrice / stonka.forwardConservativeGrahamFormulaNumber) - (stonkb.latestPrice / stonkb.forwardConservativeGrahamFormulaNumber);
+	}
+
+	function sortByCompanyName (a, b) {
+		if (a.companyName > b.companyName) {
+			return 1;
+	}
+	if (b.companyName > a.companyName) {
+			return -1;
+	}
+	return 0;
+	}
+
+	const handleSelectSort = e => setSortValue(+e.target.value);
+
+	useEffect(() => {
+		const stonksToSort = sortedStonks.slice();
+		if (sortValue === 2) setSortedStonks(stonksToSort.sort(sortByUndervalued))
+		else if (sortValue === 0) setSortedStonks(stonks)
+		else if (sortValue === 1) setSortedStonks(stonksToSort.sort(sortByCompanyName))
+	}, [sortValue])
+
+	return (
+		<>
+		<div>
+			<label>Sort</label>
+			<span className="sort">
+				<select onChange={handleSelectSort}>
+						{options.map((option, index) => <option key={`option-${index}`} value={index}>{option}</option>)}
+				</select>
+			</span>
+		</div>
+		{sortedStonks.map((stonk, index) => {
+			return stonk.priceAsPercentOfNCAV > 0 &&
+				stonk.priceAsPercentOfNCAV < 100 ? (
+				<NCAVStonk
+					key={`stonk-${index}`}
+					{...{ ...stonk, removeStonk }}
+				/>
+			) : (
+				<Stonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
+			);
+		})}
+		</>
+	)	
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,7 @@
 import React, { useContext } from "react";
-import Stonk from "../components/Stonk";
-import NCAVStonk from "../components/NCAVStonk";
 import StonkSkeleton from "../components/StonkSkeleton";
 import UserContext from "../data/context/UserContext";
+import StonkCards from "../components/StonkCards";
 
 export default function StonksDashboard() {
   const context = useContext(UserContext);
@@ -18,17 +17,7 @@ export default function StonksDashboard() {
         ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item => (
             <StonkSkeleton key={`skeleton-${item}`} />
           ))
-        : stonks.map((stonk, index) => {
-            return stonk.priceAsPercentOfNCAV > 0 &&
-              stonk.priceAsPercentOfNCAV < 100 ? (
-              <NCAVStonk
-                key={`stonk-${index}`}
-                {...{ ...stonk, removeStonk }}
-              />
-            ) : (
-              <Stonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
-            );
-          })}
+        : <StonkCards stonks={stonks} options={["By Ticker", "By Co. Name", "By Undervalued"]} />}
     </main>
   );
 }
