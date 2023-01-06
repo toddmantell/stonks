e0973cee609290953fd7da5d5a34eb3119082d02
todder02 <tmantell99@yahoo.com>
2023-01-05 19:28:37 -0600

    added ability to sort by % change desc; fixed undervalued sorting bug

--- src/components/StonkCards.js
+++ src/components/StonkCards.js
@@ -9,21 +9,21 @@ export default function StonkCards({stonks = [], options = [], removeStonk = () 
 	const [sortedStonks, setSortedStonks] = useState(stonks);
 
 	function sortByUndervalued (stonka, stonkb) {
-		return checkForNonNegativeValue(+stonka.forwardConservativeGrahamFormulaNumber, +stonkb.forwardConservativeGrahamFormulaNumber) && (stonka.latestPrice / stonka.forwardConservativeGrahamFormulaNumber) - (stonkb.latestPrice / stonkb.forwardConservativeGrahamFormulaNumber);
+		return (+stonka.latestPrice / stonka.forwardConservativeGrahamFormulaNumber) - (+stonkb.latestPrice / stonkb.forwardConservativeGrahamFormulaNumber);
 	}
 
-	function checkForNonNegativeValue(stonkValue1, stonkValue2) {
-		if (stonkValue1 > 0 && stonkValue2 > 0) return true; 
-	}
-
-	function sortByCompanyName (a, b) {
-		if (a.companyName > b.companyName) {
+	function sortByCompanyName (stonka, stonkb) {
+		if (stonka.companyName > stonkb.companyName) {
 			return 1;
 	}
-	if (b.companyName > a.companyName) {
+	if (stonkb.companyName > stonka.companyName) {
 			return -1;
 	}
 	return 0;
+	}
+
+	function sortByPercentageLost(stonka, stonkb) {
+		return +stonka.changePercent - +stonkb.changePercent;
 	}
 
 	const handleSelectSort = e => setSortValue(+e.target.value);
@@ -33,6 +33,7 @@ export default function StonkCards({stonks = [], options = [], removeStonk = () 
 		if (sortValue === 2) setSortedStonks(stonksToSort.sort(sortByUndervalued))
 		else if (sortValue === 0) setSortedStonks(stonks)
 		else if (sortValue === 1) setSortedStonks(stonksToSort.sort(sortByCompanyName))
+		else if (sortValue === 3) setSortedStonks(stonksToSort.sort(sortByPercentageLost))
 	}, [sortValue])
 
 	return (
