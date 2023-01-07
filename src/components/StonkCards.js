55521c505009bb1c9b03cf1a11e4ba2570c19b91
todder02 <tmantell99@yahoo.com>
2023-01-06 20:10:36 -0600

    cleaned up by using the OPTIONS enum for dropdown values and using switch case in place of if/else if

--- src/components/StonkCards.js
+++ src/components/StonkCards.js
@@ -1,42 +1,37 @@
-/* eslint-disable */
-import React, {useState, useReducer} from 'react';
+import React, { useReducer } from "react";
 import Stonk from "./Stonk";
 import NCAVStonk from "./NCAVStonk";
-import reducer from "./StonkCardsReducer";
+import reducer, { OPTIONS } from "./StonkCardsReducer";
 
-export default function StonkCards({stonks = [], removeStonk = () => ''}) {
+export default function StonkCards({ stonks = [], removeStonk = () => "" }) {
+  const [state, dispatch] = useReducer(reducer, { localStonks: stonks });
 
-	const [sortValue, setSortValue] = useState(-1);
-	const [state, dispatch] = useReducer(reducer, { localStonks: stonks })
+  const handleSelectSort = (e) => dispatch({ sortValue: +e.target.value });
 
-	console.log('state:', state);
-	
+  const dropdownOtions = Object.keys(OPTIONS);
 
-	const handleSelectSort = e => dispatch({sortValue: +e.target.value});
-
-	const options = ["By Ticker", "By Co. Name", "By Undervalued", "By % Change"];
-
-	return (
-		<>
-		<div>
-			<label>Sort</label>
-			<span className="sort">
-				<select onChange={handleSelectSort}>
-						{options.map((option, index) => <option key={`option-${index}`} value={index}>{option}</option>)}
-				</select>
-			</span>
-		</div>
-		{state.localStonks.map((stonk, index) => {
-			return stonk.priceAsPercentOfNCAV > 0 &&
-				stonk.priceAsPercentOfNCAV < 100 ? (
-				<NCAVStonk
-					key={`stonk-${index}`}
-					{...{ ...stonk, removeStonk }}
-				/>
-			) : (
-				<Stonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
-			);
-		})}
-		</>
-	)	
+  return (
+    <>
+      <div>
+        <label>Sort</label>
+        <span className="sort">
+          <select onChange={handleSelectSort}>
+            {dropdownOtions.map((option, index) => (
+              <option key={`option-${index}`} value={index}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </span>
+      </div>
+      {state.localStonks.map((stonk, index) => {
+        return stonk.priceAsPercentOfNCAV > 0 &&
+          stonk.priceAsPercentOfNCAV < 100 ? (
+          <NCAVStonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
+        ) : (
+          <Stonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
+        );
+      })}
+    </>
+  );
 }
--- src/components/StonkCardsReducer.js
+++ src/components/StonkCardsReducer.js
@@ -1,42 +1,54 @@
 export const OPTIONS = {
-	BY_TICKER: 0,
-	BY_CO_NAME: 1,
-	BY_UNDERVALUED: 2,
-	BY_PERCENTAGE: 3
-}
+  "By Ticker": 0,
+  "By Company Name": 1,
+  "By Undervalued": 2,
+  "By Percentage": 3,
+};
 
 export default function reducer(state, action) {
-	const stonksToSort = state.localStonks.slice();
-	if (action.sortValue === OPTIONS.BY_UNDERVALUED) return { localStonks: stonksToSort.sort(sortByUndervalued) };
-	else if (action.sortValue === OPTIONS.BY_TICKER) return { localStonks: stonksToSort.sort(sortByTicker) };
-	else if (action.sortValue === OPTIONS.BY_CO_NAME) return { localStonks: stonksToSort.sort(sortByCompanyName) };
-	else if (action.sortValue === OPTIONS.BY_PERCENTAGE) return { localStonks: stonksToSort.sort(sortByPercentageLost) };
+  const stonksToSort = state.localStonks.slice();
+
+  switch (action.sortValue) {
+    case OPTIONS["By Undervalued"]:
+      return { localStonks: stonksToSort.sort(sortByUndervalued) };
+    case OPTIONS["By Ticker"]:
+      return { localStonks: stonksToSort.sort(sortByTicker) };
+    case OPTIONS["By Company Name"]:
+      return { localStonks: stonksToSort.sort(sortByCompanyName) };
+    case OPTIONS["By Percentage"]:
+      return { localStonks: stonksToSort.sort(sortByPercentageLost) };
+    default:
+      break;
+  }
 }
 
-function sortByUndervalued (stonka, stonkb) {
-	return (+stonka.latestPrice / stonka.forwardConservativeGrahamFormulaNumber) - (+stonkb.latestPrice / stonkb.forwardConservativeGrahamFormulaNumber);
+function sortByUndervalued(stonka, stonkb) {
+  return (
+    +stonka.latestPrice / stonka.forwardConservativeGrahamFormulaNumber -
+    +stonkb.latestPrice / stonkb.forwardConservativeGrahamFormulaNumber
+  );
 }
 
 function sortByTicker(stonka, stonkb) {
-	if (stonka.symbol > stonkb.symbol) {
-		return 1;
-	}
-	if (stonkb.symbol > stonka.symbol) {
-		return -1;
-	}
-	return 0;
+  if (stonka.symbol > stonkb.symbol) {
+    return 1;
+  }
+  if (stonkb.symbol > stonka.symbol) {
+    return -1;
+  }
+  return 0;
 }
 
-function sortByCompanyName (stonka, stonkb) {
-	if (stonka.companyName > stonkb.companyName) {
-		return 1;
-	}
-	if (stonkb.companyName > stonka.companyName) {
-		return -1;
-	}
-	return 0;
+function sortByCompanyName(stonka, stonkb) {
+  if (stonka.companyName > stonkb.companyName) {
+    return 1;
+  }
+  if (stonkb.companyName > stonka.companyName) {
+    return -1;
+  }
+  return 0;
 }
 
 function sortByPercentageLost(stonka, stonkb) {
-	return +stonka.changePercent - +stonkb.changePercent;
+  return +stonka.changePercent - +stonkb.changePercent;
 }
