6b4cfd183501fc494a7c6af72d0fac3b7e70cb33
Todd Mantell <toddmantell@gmail.com>
2023-01-13 13:04:12 -0600

    Reducer for sorting (#41)
    * created reducer and extracted it plus helpers to a separate file
    
    * cleaned up by using the OPTIONS enum for dropdown values and using switch case in place of if/else if
    
    Co-authored-by: todder02 <tmantell99@yahoo.com>

--- src/components/StonkCards.js
+++ src/components/StonkCards.js
@@ -1,64 +1,37 @@
-/* eslint-disable */
-import React, {useState, useEffect} from 'react';
+import React, { useReducer } from "react";
 import Stonk from "./Stonk";
 import NCAVStonk from "./NCAVStonk";
+import reducer, { OPTIONS } from "./StonkCardsReducer";
 
-export default function StonkCards({stonks = [], removeStonk = () => ''}) {
+export default function StonkCards({ stonks = [], removeStonk = () => "" }) {
+  const [state, dispatch] = useReducer(reducer, { localStonks: stonks });
 
-	const [sortValue, setSortValue] = useState(-1);
-	const [sortedStonks, setSortedStonks] = useState(stonks);
+  const handleSelectSort = (e) => dispatch({ sortValue: +e.target.value });
 
-	function sortByUndervalued (stonka, stonkb) {
-		return (+stonka.latestPrice / stonka.forwardConservativeGrahamFormulaNumber) - (+stonkb.latestPrice / stonkb.forwardConservativeGrahamFormulaNumber);
-	}
+  const dropdownOtions = Object.keys(OPTIONS);
 
-	function sortByCompanyName (stonka, stonkb) {
-		if (stonka.companyName > stonkb.companyName) {
-			return 1;
-	  }
-	  if (stonkb.companyName > stonka.companyName) {
-			return -1;
-	  }
-	  return 0;
-	}
-
-	function sortByPercentageLost(stonka, stonkb) {
-		return +stonka.changePercent - +stonkb.changePercent;
-	}
-
-	const handleSelectSort = e => setSortValue(+e.target.value);
-
-	useEffect(() => {
-		const stonksToSort = sortedStonks.slice();
-		if (sortValue === 2) setSortedStonks(stonksToSort.sort(sortByUndervalued))
-		else if (sortValue === 0) setSortedStonks(stonks)
-		else if (sortValue === 1) setSortedStonks(stonksToSort.sort(sortByCompanyName))
-		else if (sortValue === 3) setSortedStonks(stonksToSort.sort(sortByPercentageLost))
-	}, [sortValue])
-
-	const options = ["By Ticker", "By Co. Name", "By Undervalued", "By % Change"];
-
-	return (
-		<>
-		<div>
-			<label>Sort</label>
-			<span className="sort">
-				<select onChange={handleSelectSort}>
-						{options.map((option, index) => <option key={`option-${index}`} value={index}>{option}</option>)}
-				</select>
-			</span>
-		</div>
-		{sortedStonks.map((stonk, index) => {
-			return stonk.priceAsPercentOfNCAV > 0 &&
-				stonk.priceAsPercentOfNCAV < 100 ? (
-				<NCAVStonk
-					key={`stonk-${index}`}
-					{...{ ...stonk, removeStonk }}
-				/>
-			) : (
-				<Stonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
-			);
-		})}
-		</>
-	)	
+  return (
+    <>
+      <div>
+        <label>Sort</label>
+        <span className="sort">
+          <select onChange={handleSelectSort}>
+            {dropdownOtions.map((option, index) => (
+              <option key={`option-${index}`} value={index}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </span>
+      </div>
+      {state.localStonks.map((stonk, index) => {
+        return stonk.priceAsPercentOfNCAV > 0 &&
+          stonk.priceAsPercentOfNCAV < 100 ? (
+          <NCAVStonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
+        ) : (
+          <Stonk key={`stonk-${index}`} {...{ ...stonk, removeStonk }} />
+        );
+      })}
+    </>
+  );
 }
