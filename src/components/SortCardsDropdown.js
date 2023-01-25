07d75471e710d396b62fa809501a5c5c580efabc
todder02 <tmantell99@yahoo.com>
2023-01-24 21:05:27 -0600

    add CSS vars; some typo cleanup and styling

--- src/components/SortCardsDropdown.js
+++ src/components/SortCardsDropdown.js
@@ -4,13 +4,13 @@ import { OPTIONS } from "./SortCardsReducer";
 export default function SortCards({ dispatch = () => "" }) {
   const handleSelectSort = (e) => dispatch({ sortValue: +e.target.value });
 
-  const dropdownOtions = Object.keys(OPTIONS);
+  const dropdownOptions = Object.keys(OPTIONS);
   return (
     <div className="sort-dropdown">
-      <label>Sort</label>
+      <label className="sort-label">Sort</label>
       <span className="sort">
         <select onChange={handleSelectSort}>
-          {dropdownOtions.map((option, index) => (
+          {dropdownOptions.map((option, index) => (
             <option key={`option-${index}`} value={index}>
               {option}
             </option>
