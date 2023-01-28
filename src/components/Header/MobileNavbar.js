5671d3e9a5c5312d8e062a308b1d7212dbb60d3a
todder02 <tmantell99@yahoo.com>
2023-01-28 09:05:41 -0600

    moved header css to header file for cleanliness; improved l&f of mobile nav

--- src/components/Header/MobileNavbar.js
+++ src/components/Header/MobileNavbar.js
@@ -5,22 +5,22 @@ import { Link } from "react-router-dom";
 export default function MobileNavbar(props) {
   return (
     <div className="mobile-header">
-      <Link to="/" className="nav-item">
+      <Link to="/">
         <span className="mobile-icon">
           <Home size={22} />
         </span>
       </Link>
-      <Link to="/dashboard" className="nav-item">
+      <Link to="/dashboard">
         <span className="mobile-icon">
           <Activity />
         </span>
       </Link>
-      <Link to="/addStonk" className="nav-item">
+      <Link to="/addStonk">
         <span className="mobile-icon">
           <Plus />
         </span>
       </Link>
-      <Link to="/irr" className="nav-item">
+      <Link to="/irr">
         <span className="mobile-icon">
           <Sliders />
         </span>
