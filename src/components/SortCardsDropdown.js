771d56f48381746f4bec878c8f0481128aed92db
todder02 <tmantell99@yahoo.com>
2023-01-28 13:48:26 -0600

    centered dashboard on mobile; some much-needed css refactoring

--- src/components/SortCardsDropdown.js
+++ src/components/SortCardsDropdown.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../data/context/UserContext";
 import { OPTIONS } from "./SortCardsReducer";
 
 export default function SortCards({ dispatch = () => "" }) {
+  const {
+    state: { isLoading },
+  } = useContext(UserContext);
   const handleSelectSort = (e) => dispatch({ sortValue: +e.target.value });
 
   const dropdownOptions = Object.keys(OPTIONS);
-  return (
+
+  return isLoading ? (
+    <div>...</div>
+  ) : (
     <div className="sort-dropdown">
       <label className="sort-label">Sort</label>
       <span className="sort">
