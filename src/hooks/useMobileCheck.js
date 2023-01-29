21771ec79be13459cc9b6169798228c327400dc8
todder02 <tmantell99@yahoo.com>
2023-01-28 20:01:07 -0600

    eslint doesn't understand React

--- src/hooks/useMobileCheck.js
+++ src/hooks/useMobileCheck.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line
 import React, { useState, useEffect } from "react";
 
 export default function useMobileCheck() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(null);
 
   useEffect(() => {
     setIsMobile(window.innerWidth <= 640);
