8edd79753d4df88a4465b6034bc97996ba391e7a
todder02 <tmantell99@yahoo.com>
2023-01-31 20:32:59 -0600

    added company logos to front of cards; placeholder app logo

--- src/components/Header/MobileHeader.js
+++ src/components/Header/MobileHeader.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default ({ pageName = "STONKS" }) => (
-  <div className="mobile-header">{pageName}</div>
+  <div className="mobile-header">
+    <div className="mobile-header-container">
+      <span>
+        <img className="mobile-header-image" src="../../../stonks.jpg" />
+      </span>
+      <span className="mobile-header-text">{pageName}</span>
+    </div>
+  </div>
 );
