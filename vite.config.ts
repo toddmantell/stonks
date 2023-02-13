cd56845b41af977d4cc07bb2bc2b56e1a8c61413
todder02 <tmantell99@yahoo.com>
2023-02-13 09:13:42 -0600

    added comment explaining

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 
+// 2/13/23: attempted to migrate from these docs: https://cathalmacdonnacha.com/migrating-from-create-react-app-cra-to-vite
+// But local desktop can only run node 12 and that didn't support esmodules like newer versions of node
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
